60d008b0983fc06bc86f725912fecb7c8ace1be8
Domen Osojnik <raiin326@gmail.com>
2018-05-04 20:22:42 +0200

    Krneki

--- App.js
+++ App.js
@@ -1,29 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { Component } from 'react';
+import { StyeSheet, Text, View, ActivityIndicator } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Image source={require('./Slike/velenje_ozadje.jpg')}>
-                    
-                    {this.props.children}
-                    
-            </Image>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  backgroundImage: {
-        flex: 1,
-        width: null,
-        height: null,
-        resizeMode: 'cover'
+export default class App extends Component {
+    
+    constructor(props)
+    {
+        super();
+        this.state = { isLoading: true, jsonResponse: '' }
     }
-});
+    
+    componentDidMount()
+    {
+	    
+        setInterval(() => this.GetApiData(), 10000);
+    }
+    
+    async GetApiData()
+    {
+	    //Prenesi podatke iz API-ja
+        return fetch ('http://oc.velenje.si/api/v1/organizations/lokalc-postaje')
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                jsonResponse: data, isLoading: false
+            });
+        })
+        .catch((error) => {
+           console.error(error); 
+        });
+    }
+    
+    render()
+    {
+        if(this.state.isLoading)
+        {
+            return (
+                <View style={{flex: 1, padding: 30}}>
+                    <ActivityIndicator size="large" color="#0000ff" />
+                </View>
+            );    
+        }
+        else
+        {
+	        return (
+                <View style={{flex: 1, padding: 30}}>
+                   <Text>
+                   Številka postaje: {this.state.jsonResponse.id.attr_value}
+                   </Text>
+                   
+                   
+                   <Text>
+                   Trenutni čas: {new Date().toLocaleString()}
+                   </Text>
+                </View>
+            );   
+        }
+    }
+}
