b227b80cd0ccbb5a58b73a0c71ca2693f8f05533
gallindic <2stskier@gmail.com>
2018-05-05 15:14:43 +0200

    APIs are being fetched and added tabed navigation

--- App.js
+++ App.js
@@ -1,8 +1,49 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, ImageBackground, Text, StyleSheet, View, ScrollView } from 'react-native';
+import { ActivityIndicator, ImageBackground, Text, StyleSheet, View, ScrollView, Button } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; // Version can be specified in package.json
+import { createBottomTabNavigator, TabBarBottom } from 'react-navigation';
 
-export default class App extends Component {
-    
+//export default class App extends Component {
+//    
+
+//
+//    render()
+//    {
+        
+//        else{
+//            return(
+//                <View style={styles.container}>
+//                    <ImageBackground 
+//                        style={styles.backgroundImage} 
+//                        source={require('./Slike/velenje_ozadje_v2.jpg')}
+//                        resizeMode='cover'
+//                        blurRadius={10}>
+//                        <ScrollView contentContainerStyle={{alignItems: 'center', justifyContent: 'center', width: '100%'}}>
+//
+//                              //Lokalc View
+//                                <View style={styles.panel}>
+//                                <Text style={styles.headline}>Številka postaje: {this.state.lokalcData.result[0].id.attr_value}</Text>
+//                                <Text style={styles.headline}>Naslov: {this.state.lokalcData.result[0].naslov.attr_value}</Text>
+//                              </View>
+//                              //Bicy View
+//                              <View style={styles.panel}>
+//                                <Text style={styles.headline}>Postaja: {this.state.bicyData[0].station.attr_value}</Text>
+//                                <Text style={styles.headline}>Število koles: {this.state.bicyData[0].available.attr_value}</Text>
+//                              </View>
+//                                <View style={styles.panel}>
+//                                <Text style={styles.headline}>Postaja: {this.state.bicyData[0].station.attr_value}</Text>
+//                                <Text style={styles.headline}>Število koles: {this.state.bicyData[0].available.attr_value}</Text>
+//                              </View>
+//                        </ScrollView>
+//                    </ImageBackground>
+//                </View>
+//            );
+//        }
+//    }
+//}
+
+
+class lokalcScreen extends Component {
     constructor(props){
         super();
         this.state = { isLoading: true, lokalcData: '', bicyData: '' }
@@ -45,8 +86,16 @@ export default class App extends Component {
         if(this.state.isLoading)
         {
             return (
-                <View style={{flex: 1, padding: 30}}>
-                    <ActivityIndicator size="large" color="#0000ff" />
+                <View style={styles.loadingContainer}>
+                    <ImageBackground 
+                        style={styles.backgroundImage, styles.loadingContainer} 
+                        source={require('./Slike/velenje_ozadje_v2.jpg')}
+                        resizeMode='cover'
+                        blurRadius={10}>
+                        
+                        <ActivityIndicator size="large" color="#fff" />
+                    </ImageBackground>
+                    
                 </View>
             );    
         }
@@ -66,16 +115,6 @@ export default class App extends Component {
                                 <Text style={styles.headline}>Številka postaje: {this.state.lokalcData.result[0].id.attr_value}</Text>
                                 <Text style={styles.headline}>Naslov: {this.state.lokalcData.result[0].naslov.attr_value}</Text>
                               </View>
-                              //Bicy View
-                              <View style={styles.panel}>
-                                <Text style={styles.headline}>Postaja: {this.state.bicyData[0].station.attr_value}</Text>
-                                <Text style={styles.headline}>Število koles: {this.state.bicyData[0].available.attr_value}</Text>
-                              </View>
-                                <View style={styles.panel}>
-                                <Text style={styles.headline}>Postaja: {this.state.bicyData[0].station.attr_value}</Text>
-                                <Text style={styles.headline}>Število koles: {this.state.bicyData[0].available.attr_value}</Text>
-                              </View>
-
                         </ScrollView>
                     </ImageBackground>
                 </View>
@@ -84,10 +123,113 @@ export default class App extends Component {
     }
 }
 
+class bicyScreen extends Component {
+  constructor(props){
+        super();
+        this.state = { isLoading: true, lokalcData: '', bicyData: '' }
+    }
+    
+    componentDidMount(){
+        setInterval(() => this.getApiData(), 5000);
+    }
+    
+    async getApiData()
+    {
+        //Prenesi podatke iz API-ja
+        
+        var lokalcPostajeAPI = 'http://oc.velenje.si/api/v1/organizations/lokalc-postaje';
+        
+        var bicyPostajeAPI = 'http://oc.velenje.si/api/v1/organizations/bicy';
+        
+        return fetch (lokalcPostajeAPI)
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                lokalcData: data,
+            });
+        }).then(()=>{
+            fetch(bicyPostajeAPI).then((response) => response.json())
+            .then((data) => {
+              this.setState({
+                 bicyData: data,
+                 isLoading: false
+              });  
+            })
+        })
+        .catch((error) => {
+           console.error(error); 
+        });
+    }
+
+    render()
+    {
+        if(this.state.isLoading)
+        {
+            return (
+                <View style={styles.loadingContainer}>
+                    <ImageBackground 
+                        style={styles.backgroundImage, styles.loadingContainer} 
+                        source={require('./Slike/velenje_ozadje_v2.jpg')}
+                        resizeMode='cover'
+                        blurRadius={10}>
+                        
+                        <ActivityIndicator size="large" color="#fff" />
+                    </ImageBackground>
+                    
+                </View>
+            );    
+        }
+        else{
+            return(
+                <View style={styles.container}>
+                    <ImageBackground 
+                        style={styles.backgroundImage} 
+                        source={require('./Slike/velenje_ozadje_v2.jpg')}
+                        resizeMode='cover'
+                        blurRadius={10}>
+                        <ScrollView contentContainerStyle={{alignItems: 'center', justifyContent: 'center', width: '100%'}}>
+                          //Bicy View
+                          <View style={styles.panel}>
+                            <Text style={styles.headline}>Postaja: {this.state.bicyData[0].station.attr_value}</Text>
+                            <Text style={styles.headline}>Število koles: {this.state.bicyData[0].available.attr_value}</Text>
+                        </View>
+                        </ScrollView>
+                    </ImageBackground>
+                </View>
+            );
+        }
+    }
+}
+
+export default createBottomTabNavigator(
+  {
+    Lokalc: { screen: lokalcScreen },
+    Bicy: { screen: bicyScreen },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Lokalc') {
+          iconName = `ios-bus${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Bicy') {
+          iconName = `ios-bicycle${focused ? '' : '-outline'}`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+  }
+);
+
+
 const styles = StyleSheet.create({
   loadingContainer: {
     flex: 1,
-      justifyContent: 'center',
+    justifyContent: 'center',
   },
   backgroundImage: {
     width: '100%',
